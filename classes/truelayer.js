ee033f10a38c39801c4f2de6fe05691d099fc339
Carina Druce <trueblue546@gmail.com>
2022-04-29 14:08:34 +0100

    all get requests working

--- classes/truelayer.js
+++ classes/truelayer.js
@@ -44,4 +44,111 @@ module.exports = class Truelayer {
       throw new Error("Error: ", error);
     }
   }
+  async fetchBalance(accountID) {
+    try {
+      const response = await axios.get(
+        `${this.url}/accounts/${accountID}/balance`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          },
+        }
+      );
+      const balance = response.data;
+      return balance;
+    } catch (error) {
+      throw new Error("Error: ", error);
+    }
+  }
+  async fetchAccountTransactions(accountID) {
+    try {
+      const response = await axios.get(
+        `${this.url}/accounts/${accountID}/transactions`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          },
+        }
+      );
+      const balance = response.data;
+      return balance;
+    } catch (error) {
+      throw new Error("Error: ", error);
+    }
+  }
+  async fetchDD(accountID) {
+    try {
+      const response = await axios.get(
+        `${this.url}/accounts/${accountID}/direct_debits`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          },
+        }
+      );
+      const balance = response.data;
+      return balance;
+    } catch (error) {
+      throw new Error("Error: ", error);
+    }
+  }
+
+  async fetchInfo(accountID) {
+    try {
+      const response = await axios.get(`${this.url}/info`, {
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+        },
+      });
+      const balance = response.data;
+      return balance;
+    } catch (error) {
+      throw new Error("Error: ", error);
+    }
+  }
+
+  async fetchSingleCard(cardID) {
+    try {
+      const response = await axios.get(`${this.url}/cards/${cardID}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+        },
+      });
+      const card = response.data;
+      return card;
+    } catch (error) {
+      throw new Error("Error: ", error);
+    }
+  }
+
+  async fetchCardBalance(cardID) {
+    try {
+      const response = await axios.get(`${this.url}/cards/${cardID}/balance`, {
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+        },
+      });
+      const card = response.data;
+      return card;
+    } catch (error) {
+      throw new Error("Error: ", error);
+    }
+  }
+
+  async fetchCardTransactions(cardID) {
+    try {
+      const response = await axios.get(
+        `${this.url}/cards/${cardID}/transactions`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          },
+        }
+      );
+      const card = response.data;
+      return card;
+    } catch (error) {
+      throw new Error("Error: ", error);
+    }
+  }
 };
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const getAllCards = async () => {
   console.log(response);
 };
 
-getAllCards();
+// getAllCards();
 
 const getAllAccounts = async () => {
   const account = new Truelayer();
@@ -16,7 +16,7 @@ const getAllAccounts = async () => {
   console.log(response);
 };
 
-getAllAccounts();
+// getAllAccounts();
 
 const getOneAccount = async (accountID) => {
   const account = new Truelayer();
@@ -24,4 +24,60 @@ const getOneAccount = async (accountID) => {
   console.log(response);
 };
 
-getOneAccount("56c7b029e0f8ec5a2334fb0ffc2fface");
+// getOneAccount("56c7b029e0f8ec5a2334fb0ffc2fface");
+
+const getBalance = async (accountID) => {
+  const account = new Truelayer();
+  const response = await account.fetchBalance(accountID);
+  console.log(response);
+};
+
+// getBalance("56c7b029e0f8ec5a2334fb0ffc2fface");
+
+const getAccountTransactions = async (accountID) => {
+  const account = new Truelayer();
+  const response = await account.fetchAccountTransactions(accountID);
+  console.log(response);
+};
+
+// getAccountTransactions("56c7b029e0f8ec5a2334fb0ffc2fface");
+
+const getDD = async (accountID) => {
+  const account = new Truelayer();
+  const response = await account.fetchDD(accountID);
+  console.log(response);
+};
+
+// getDD("56c7b029e0f8ec5a2334fb0ffc2fface");
+
+const getInfo = async () => {
+  const account = new Truelayer();
+  const response = await account.fetchInfo();
+  console.log(response);
+};
+
+// getInfo();
+
+const getSingleCard = async (cardID) => {
+  const card = new Truelayer();
+  const response = await card.fetchSingleCard(cardID);
+  console.log(response);
+};
+
+// getSingleCard("328f557c68aebd532cbbd05ce5bcb6c8");
+
+const getCardBalance = async (cardID) => {
+  const card = new Truelayer();
+  const response = await card.fetchCardBalance(cardID);
+  console.log(response);
+};
+
+// getCardBalance("328f557c68aebd532cbbd05ce5bcb6c8");
+
+const getCardTransactions = async (cardID) => {
+  const card = new Truelayer();
+  const response = await card.fetchCardTransactions(cardID);
+  console.log(response);
+};
+
+getCardTransactions("328f557c68aebd532cbbd05ce5bcb6c8");
